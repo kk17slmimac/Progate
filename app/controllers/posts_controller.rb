17ab07bbd5627acebb739b95ab680134b3a0f922
EC2 Default User <ec2-user@ip-172-31-4-110.us-east-2.compute.internal>
2018-10-03 14:01:48 +0000

    バリデーション追加直後

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,36 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all.order(created_at: :desc)
-    #もしくはasc(昇順)
-    
   end
   
   def show
-    @post = Post.find_by(id:params[:id])
-    
+    @post = Post.find_by(id: params[:id])
   end
-  
   
   def new
-   redirect_to("posts/index")
   end
- 
+  
   def create
-    @post=Post.new(content:params[:content])
+    @post = Post.new(content: params[:content])
     @post.save
-    redirect_to("posts/index")
+    redirect_to("/posts/index")
   end
-
-
+  
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+  
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
+    redirect_to("/posts/index")
+  end
+  
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to("/posts/index")
+  end
+  
 end
- 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  # contentカラムに対して、空の投稿を制限するバリデーションを作成してください
+  validates :content, {presence: true}
+    #検証するカラム名 , 検証する内容
+  
+  
 end
