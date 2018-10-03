9c08741348e88545b3689374390bc5e31e0048ff
EC2 Default User <ec2-user@ip-172-31-4-110.us-east-2.compute.internal>
2018-10-03 08:05:04 +0000

    progate3完了時点

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
+    #もしくはasc(昇順)
+    
   end
   
   def show
@@ -11,8 +13,14 @@ class PostsController < ApplicationController
   
   def new
    redirect_to("posts/index")
-  
   end
+ 
+  def create
+    @post=Post.new(content:params[:content])
+    @post.save
+    redirect_to("posts/index")
+  end
+
 
 end
  
