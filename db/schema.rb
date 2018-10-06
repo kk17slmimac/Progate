d4fae942428c875f13a936b61df06f93c0799455
EC2 Default User <kk17slmimac@gmail.com>
2018-10-06 12:27:57 +0000

    カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_04_131505) do
+ActiveRecord::Schema.define(version: 2018_10_06_121133) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2018_10_04_131505) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
   end
 
 end
