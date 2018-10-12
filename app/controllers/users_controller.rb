0d23d0e611244f906d63f80dc9b8fe454cf9c9c9
EC2 Default User <kk17slmimac@gmail.com>
2018-10-12 12:53:47 +0000

    ログイン機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,6 +56,15 @@ class UsersController < ApplicationController
   
   def login
   
+   @user = User.find_by(email: params[:email],password: params[:password])
+
+  if @user
+    # @userが存在するかどうかを判定するif文を作成してください
+    flash[:notice] ="ログインしました"
+    redirect_to("/posts/index")
+  else
+    render("users/login_form")
+  end
   end
   
 end
