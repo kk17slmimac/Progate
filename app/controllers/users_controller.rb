6364cad3d46b1251f44e66911ae1381f6f67529c
EC2 Default User <kk17slmimac@gmail.com>
2018-10-13 04:35:01 +0000

    8章終了時点

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   before_action :authenticate_user, {only: [:index, :show, :edit, :update]}
-  # before_actionにforbid_login_userメソッドを指定してください
   before_action :forbid_login_user, {only: [:new, :create, :login_form, :login]}
+  # before_actionにensure_correct_userメソッドを指定してください
+  before_action :ensure_correct_user, {only: [:edit, :update]}
   
   def index
     @users = User.all
@@ -75,6 +76,14 @@ class UsersController < ApplicationController
     session[:user_id] = nil
     flash[:notice] = "ログアウトしました"
     redirect_to("/login")
+  end
+  
+  # ensure_correct_userを定義してください
+  def ensure_correct_user
+    if @current_user.id != params[:id].to_i
+      flash[:notice] = "権限がありません"
+      redirect_to("/posts/index")
+    end
   end
   
 end
