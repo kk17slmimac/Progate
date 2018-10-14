77be32d335b7beade2a243adbb48c9f730f06428
EC2 Default User <kk17slmimac@gmail.com>
2018-10-14 05:05:40 +0000

    投稿とユーザーを紐付ける為にカラム追加等

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
-  # contentカラムに対して、空の投稿を制限するバリデーションを作成してください
-  validates :content, {presence: true , length: {maximum:140}}
-    #検証するカラム名 , 検証する内容
+  validates :content, {presence: true, length: {maximum: 140}}
+  # user_idカラムにバリデーションを追加してください
+  validates :user_id, {presence: true}
+  
 end
