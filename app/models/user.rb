230246aa0f7b871381cee58df816e55d4d34f5f1
EC2 Default User <kk17slmimac@gmail.com>
2018-10-15 14:11:27 +0000

    Ⅸ章終了時点

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
-    validates :password,{presence:true}
+  validates :name, {presence: true}
+  validates :email, {presence: true, uniqueness: true}
+  validates :password, {presence: true}
+  
+  def posts
+    return Post.where(user_id: self.id)
+  end
+  
 end
